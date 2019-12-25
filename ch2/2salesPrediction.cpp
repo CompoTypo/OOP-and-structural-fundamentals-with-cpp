433659429051a77dd5a05f210260bac8e5320499
DoggyBagBrunch <andrew.ja.jensen48@gmail.com>
2019-12-25 02:19:40 -0600

    some program descriptions added

--- ch2/2salesPrediction.cpp
+++ ch2/2salesPrediction.cpp
@@ -1,3 +1,12 @@
+/*
+The East Coast sales division of a company generates 
+    58 percent of total sales. 
+Based on that percentage, 
+    write a program that will predict how much the East Coast division
+	will generate if the company has $8.6 million in sales this year.
+*/
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
--- ch2/3salesTax.cpp
+++ ch2/3salesTax.cpp
@@ -1,3 +1,9 @@
+/*
+Write a program that will 
+    compute the total sales tax on a $95 purchase. 
+**Assume the state sales tax is 4 percent and the county sales tax is 2 percent.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
